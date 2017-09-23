7bdc903bc5cc59bb83772bcc919a52faba04ec05
Hockma Solutions <agomez.barron@gmail.com>
2017-09-23 08:37:54 -0500

    Inicio de App

---
+++ views/login/FrmLogin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agomez
+ * Date: 23/09/2017
+ * Time: 12:35 AM
+ */
+
+?>
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Ikro Sistemas</title>
+</head>
+<body class="full">
+
+
+<div class="contenedor">
+
+
+</div>
+
+
+</body>
+</html>
